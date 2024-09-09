ce5e597448ebe32c5c13ed14791b015051c65252
HenriqueLDSa <rique.ldsa@gmail.com>
2024-09-09 02:37:09 -0400

    adding functionality to get cookies when loading dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,23 @@
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function checkLogin() {
+    let userId = getCookie("userId");
+    if (!userId) {
+        window.location.href = "index.html";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", checkLogin);
+
 const urlBase = 'http://team27poosd.site/LAMPAPI';
 const extension = 'php';
 
