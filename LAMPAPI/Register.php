e4ffec3d8aaa93659a8065a7b678450b6605640b
HenriqueLDSa <rique.ldsa@gmail.com>
2024-09-12 13:33:34 -0400

    allowing access from all origins on php endpoints v4

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,6 +1,13 @@
 <?php
     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
 
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+    
 //view errors in browser (for debugging purposes)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
