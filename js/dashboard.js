cc114df68b5bb411b99d81213e010ec0dda51df8
Randy Nguyen <randy22nguyen@gmail.com>
2024-09-18 17:29:03 -0400

    Revert "Tryna change the placeholder to editing the actual contact"
    This reverts commit 89cb8e030143b21839a0eda49893b7b4ff074cdb.

--- js/dashboard.js
+++ js/dashboard.js
@@ -351,11 +351,10 @@ contactList.addEventListener('click', (event) => {
         });
 
         contactDataModal.style.display = 'flex';
-        // document.getElementById('firstNameInput').placeholder = contactFirstNameElement.textContent;
-        // document.getElementById('lastNameInput').placeholder = contactLastNameElement.textContent;
-        // document.getElementById('numberInput').placeholder = contactNumberElement.textContent;
-        // document.getElementById('emailInput').placeholder = contactEmailElement.textContent;
-        document.getElementById('firstNameInput').innerHTML = "Value = " + "'" + contactFirstNameElement.textContent + "'";
+        document.getElementById('firstNameInput').placeholder = contactFirstNameElement.textContent;
+        document.getElementById('lastNameInput').placeholder = contactLastNameElement.textContent;
+        document.getElementById('numberInput').placeholder = contactNumberElement.textContent;
+        document.getElementById('emailInput').placeholder = contactEmailElement.textContent;
 
         document.body.classList.add('modal-open');
 
