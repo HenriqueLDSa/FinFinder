56c9e25eada737b38830c23a4c036e88523bae28
HenriqueLDSa <rique.ldsa@gmail.com>
2024-09-20 18:39:41 -0400

    deleting commented-out code

--- js/dashboard.js
+++ js/dashboard.js
@@ -304,9 +304,9 @@ function handleNewContactSubmit() {
     if (!firstName || !lastName || !phoneNum || !emailAdd) {
         document.getElementById("contactResult").innerHTML = "Please fill out all fields.";
     }else if(emailRegex.test(emailAdd) == false){
-        document.getElementById("contactResult").innerHTML = "Not an actual email address.";
+        document.getElementById("contactResult").innerHTML = "Invalid email address.";
     }else if(phoneRegex.test(phoneNum) == false){
-        document.getElementById("contactResult").innerHTML = "Not an actual phone number.";
+        document.getElementById("contactResult").innerHTML = "Invalid phone number.";
     } else {
         addContact(firstName, lastName, phoneNum, emailAdd);
         contactDataModal.style.display = 'none';
@@ -330,9 +330,9 @@ function handleEditContactSubmit(contactID) {
     if (!firstName || !lastName || !phoneNum || !emailAdd) {
         document.getElementById("contactResult").innerHTML = "Please fill out all fields.";
     }else if(emailRegex.test(emailAdd) == false){
-        document.getElementById("contactResult").innerHTML = "Not an actual email address.";
+        document.getElementById("contactResult").innerHTML = "Invalid email address.";
     }else if(phoneRegex.test(phoneNum) == false){
-        document.getElementById("contactResult").innerHTML = "Not an actual phone number.";
+        document.getElementById("contactResult").innerHTML = "Invalid phone number.";
     } else {
         editContact(contactID, firstName, lastName, phoneNum, emailAdd);
         contactDataModal.style.display = 'none';
@@ -382,7 +382,6 @@ contactList.addEventListener('click', (event) => {
 
         document.body.classList.add('modal-open');
 
-
         submitDataBtn.removeEventListener('click', handleNewContactSubmit);  
         submitDataBtn.removeEventListener('click', handleEditContactSubmit); 
         submitDataBtn.addEventListener('click', () => handleEditContactSubmit(contactID)); 
@@ -429,209 +428,3 @@ exitBtn.addEventListener('click', function() {
     numberInput.value = '';
     emailInput.value = '';
 });
-
-// contactList.addEventListener('click', (event) => {
-//     if (event.target && event.target.id === 'edit-icon') {
-//         const contactItem = event.target.closest('.contact-item');
-        
-//         const contactNameElement = contactItem.querySelector('#contact-name');
-//         const contactFirstNameElement = contactNameElement.querySelector("#first-name");
-//         const contactLastNameElement = contactNameElement.querySelector("#last-name");
-//         const contactNumberElement = contactItem.querySelector('#contact-number');
-//         const contactEmailElement = contactItem.querySelector('#contact-email');
-
-//         let contactID;
-
-//         userContacts.forEach(contact => {
-//             if (contact.FirstName === contactFirstNameElement.textContent && 
-//                 contact.LastName === contactLastNameElement.textContent && 
-//                 contact.Phone === contactNumberElement.textContent && 
-//                 contact.Email === contactEmailElement.textContent) {
-//                 contactID = contact.ID;
-//                 console.log(contactID);
-//             }
-//         });
-        
-//         contactDataModal.style.display = 'flex';
-//         document.body.classList.add('modal-open');
-
-//         submitDataBtn.onclick = function() {
-//             const firstName = firstNameInput.value;
-//             const lastName = lastNameInput.value;
-//             const phoneNum = numberInput.value;
-//             const emailAdd = emailInput.value;
-        
-//             if (!firstName) {
-//                 alert('Please enter the first name.');
-//             }
-//             else if (!lastName) {
-//                 alert('Please enter the last name.');
-//             }    
-//             else if (!phoneNum) {
-//                 alert('Please enter the phone number.');
-//             }
-//             else if (!emailAdd) {
-//                 alert('Please enter the email address.');
-//             }
-//             else {
-//                 editContact(contactID, firstName, lastName, phoneNum, emailAdd);
-//                 contactDataModal.style.display = 'none';
-//                 document.body.classList.remove('modal-open');
-//                 firstNameInput.value = '';
-//                 lastNameInput.value = ''
-//                 numberInput.value = '';
-//                 emailInput.value = '';
-//             }
-//         };
-//     }
-// });
-
-//testing
-
-//editIcon.addEventListener('click', function() {      
-    
-    // document.getElementById('edit-contact-modal').style.display = 'flex';
-    // document.body.classList.add('modal-open');
-
-    // // Save button event to trigger editContacts
-    // document.getElementById('submitEditBtn').onclick = function() {
-    //     const updatedFirstName = document.getElementById('FirstNameInput').value;
-    //     const updatedLastName = document.getElementById('lastNameInput').value; 
-    //     const updatedNumber = document.getElementById('NumberInput').value;
-    //     const updatedEmail = document.getElementById('EmailInput').value;
-        
-    //     //we need the id to track which contact we're editing. Each contact has a unique userID that we can use to track it
-
-    //     //FOR HENRIQUE (Uncomment editContact(jsonObject[i].ID, updatedFirstName, updatedLastName, updatedNumber, updatedEmail) )
-    //     //editContact(jsonObject[i].ID, updatedFirstName, updatedLastName, updatedNumber, updatedEmail);
-
-    //     document.getElementById('edit-contact-modal').style.display = 'none';
-    //     document.body.classList.remove('modal-open');
-    // };
-    
-    // // Exit button event in editContacts
-    // document.getElementById('exitEditBtn').onclick = function() {
-    //     document.getElementById('edit-contact-modal').style.display = 'none';
-    //     document.body.classList.remove('modal-open'); 
-    // };
-
-//});
-
-// function loadContacts() {
-//     let newContactObj = 
-//     {
-//         "userID": userId
-//     };
-
-//     let jsonPayload = JSON.stringify(newContactObj);
-
-//     let url = urlBase + '/getContacts.' + extension;
-
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", url, true);
-//     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-//     try {
-//         xhr.onreadystatechange = function() {
-//             if (this.readyState == 4 && this.status == 200) {
-//                 let jsonObject = JSON.parse(xhr.responseText);
-                
-//                 //log entire contacts object
-//                 console.log("Contacts:", jsonObject);
-                
-//                 // Clear the current contact list
-//                 const contactContainer = document.querySelector(".contact-item-container");
-//                 contactContainer.innerHTML = "";
-
-//                 // Loop through each contact and display it
-//                 for (let i = 0; i < jsonObject.length; i++) {
-//                     //MAKE SURE JSON OBJECTS MATCH CONTACTS COLUMN NAMES IN DATABASE 
-//                     const newContactElement = document.createElement("div");
-//                     newContactElement.className = "contact-item";
-                
-//                     const contactName = document.createElement("span");
-//                     contactName.id = "contact-name";
-//                     contactName.textContent = jsonObject[i].FirstName + ' ' + jsonObject[i].LastName;
-//                     newContactElement.appendChild(contactName);
-                
-//                     const contactNumber = document.createElement("span");
-//                     contactNumber.id = "contact-number";
-//                     contactNumber.textContent = jsonObject[i].Phone; 
-//                     newContactElement.appendChild(contactNumber);
-                
-//                     const contactEmail = document.createElement("span");
-//                     contactEmail.id = "contact-email";
-//                     contactEmail.textContent = jsonObject[i].Email;
-//                     newContactElement.appendChild(contactEmail);
-
-//                     //edit and delete buttons
-//                     const contactMods = document.createElement("div");
-//                     contactMods.className = "contact-mods";
-//                     const editIcon = document.createElement("i");
-//                     editIcon.className = "far fa-edit";
-//                     const trashIcon = document.createElement("i");
-//                     trashIcon.className = "fa fa-trash-alt";
-//                     contactMods.appendChild(editIcon);
-//                     contactMods.appendChild(trashIcon);
-//                     newContactElement.appendChild(contactMods);
-                
-//                     // Append the new contact element to the contact container
-//                     const contactContainer = document.querySelector(".contact-item-container");
-//                     contactContainer.appendChild(newContactElement);
-                    
-//                     //EDIT CONTACTS
-//                     editIcon.addEventListener('click', function() {
-                    
-//                         // Open the edit modal and populate fields with current contact info
-//                         //jsonObject[i] pulls contact we're editing from the list we're iterating through (it's in the for loop)
-//                         document.getElementById('FirstNameInput').value = jsonObject[i].FirstName;
-//                         document.getElementById('lastNameInput').value = jsonObject[i].LastName;
-//                         document.getElementById('NumberInput').value = jsonObject[i].Phone;
-//                         document.getElementById('EmailInput').value = jsonObject[i].Email;
-
-//                         // Open the edit modal
-//                         document.getElementById('edit-contact-modal').style.display = 'flex';
-//                         document.body.classList.add('modal-open');
-
-//                         // Save button event to trigger editContacts
-//                         document.getElementById('submitEditBtn').onclick = function() {
-//                             const updatedFirstName = document.getElementById('FirstNameInput').value;
-//                             const updatedLastName = document.getElementById('lastNameInput').value; 
-//                             const updatedNumber = document.getElementById('NumberInput').value;
-//                             const updatedEmail = document.getElementById('EmailInput').value;
-                            
-//                             //we need the id to track which contact we're editing. Each contact has a unique userID that we can use to track it
-
-//                             //FOR HENRIQUE (Uncomment editContact(jsonObject[i].ID, updatedFirstName, updatedLastName, updatedNumber, updatedEmail) )
-//                             //editContact(jsonObject[i].ID, updatedFirstName, updatedLastName, updatedNumber, updatedEmail);
-
-//                             // Close the modal
-//                             document.getElementById('edit-contact-modal').style.display = 'none';
-//                             document.body.classList.remove('modal-open');
-//                         };
-                        
-//                         // Exit button event in editContacts
-//                         document.getElementById('exitEditBtn').onclick = function() {
-//                             document.getElementById('edit-contact-modal').style.display = 'none';
-//                             document.body.classList.remove('modal-open'); 
-//                         };
-
-//                     });
-
-
-//                     // DELETE CONTACTS
-//                     trashIcon.addEventListener('click', function() {
-//                         //we also need id to erase the contact from the table 
-
-//                         //FOR HENRIQUE (Uncomment deleteContact(jsonObject[i].ID)
-//                         //deleteContact(jsonObject[i].ID)
-//                     });
-                
-//                 }
-//             }
-//         };
-//         xhr.send(jsonPayload);
-//     } catch (err) {
-//         console.log("Error loading contacts: " + err.message);
-//     }
-// }
